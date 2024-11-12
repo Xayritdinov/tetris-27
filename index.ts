d191138a97bb7b9a94c1bb6c5815111c0be51569
Xayritdinov <rafa.chgm@gmail.com>
2024-11-12 18:44:57 +0500

    DZ № 27

--- index.ts
+++ index.ts
@@ -1,106 +1,350 @@
-import { shapes } from "./shapes"
+// Импортируем интерфейсы и фигуры для игры
+import { IShapes } from './interfaces';
+import { shapes } from './shapes';
 
+// === Объявление функций ===
+
+// Функция для отрисовки игрового поля
 function drawTetrisPlayground(x: number, y: number, target: HTMLDivElement) {
-    if (x <= 0 || y <= 0) throw new Error('x and y cannot be negative')
-    if (target.children.length) throw new Error('Aborted: target element should be empty')
+  if (x <= 0 || y <= 0) throw new Error('x and y cannot be negative');
 
-    for (let rowsCount = 0; rowsCount < y; rowsCount++) {
-        const row = document.createElement('div')
-        row.className = 'row'
-        row.dataset['row'] = rowsCount.toString()
-        row.style.transform = `translateY(${-rowsCount}px)`
+  if (target.children.length)
+    throw new Error('Aborted: target element should be empty');
 
-        for (let cellsCount = 0; cellsCount < x; cellsCount++) {
-            const cell = document.createElement('div')
-            cell.className = 'cell'
-            cell.dataset['cell'] = cellsCount.toString()
-            cell.style.transform = `translateX(${-cellsCount}px)`
-            row.append(cell)
-        }
+  for (let rowsCount = 0; rowsCount < y; rowsCount++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    row.dataset['row'] = rowsCount.toString();
 
-        target.append(row)
+    for (let cellsCount = 0; cellsCount < x; cellsCount++) {
+      const cell = document.createElement('div');
+      cell.className = 'cell';
+      cell.dataset['cell'] = cellsCount.toString();
+      row.append(cell);
     }
+
+    target.append(row);
+  }
 }
 
-const tetrisPlaygroundTarget = document.querySelector('.tetris-playground') as (HTMLDivElement | null)
+// Функция для отображения зафиксированных блоков на игровом поле
+function renderFixedBlocks() {
+  for (let row = 0; row < playground.length; row++) {
+    for (let col = 0; col < playground[row].length; col++) {
+      const cell = tetrisPlaygroundTarget?.children[row]?.children[
+        col
+      ] as HTMLDivElement;
+      if (!cell) continue;
 
-try {
-    if (tetrisPlaygroundTarget) {
-        drawTetrisPlayground(10, 20, tetrisPlaygroundTarget)
+      // Если блок зафиксирован, отобразим его цвет, иначе очистим ячейку
+      if (playground[row][col]) {
+        cell.style.backgroundColor = 'grey';
+      } else {
+        cell.style.backgroundColor = '';
+      }
     }
-} catch (e) {
-    console.log(e.message)
+  }
 }
 
-const shapeKeys = Object.keys(shapes)
-
-const shapeKeyIndex = Math.floor(Math.random() * shapeKeys.length)
-const shapeKey = shapeKeys[shapeKeyIndex]
-
-let currentShape = shapes[shapeKey as keyof typeof shapes]
-
+// Функция для отображения движущейся фигуры на игровом поле
 function renderShape() {
-    const rowsToColor = currentShape.shape.length
-    const cellsToColor = currentShape.shape[0].length
+  const rowsToColor = currentShape.shape.length;
+  const cellsToColor = currentShape.shape[0].length;
 
-    const centerX = Math.floor((10 - cellsToColor) / 2)
-    const centerY = 0
+  for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
+      if (currentShape.shape[rowIndex][cellIndex]) {
+        const x = currentX + cellIndex;
+        const y = currentY + rowIndex;
 
-    for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
-        const row = tetrisPlaygroundTarget.children[rowIndex + centerY] as HTMLDivElement
-
-        for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
-            const cell = row.children[cellIndex + centerX] as HTMLDivElement
-            if (currentShape.shape[rowIndex][cellIndex]) {
-                cell.style.backgroundColor = currentShape.color
-            }
+        // Проверяем, не выходит ли за границы
+        if (x >= 0 && x < 10 && y >= 0 && y < 20) {
+          const cell = tetrisPlaygroundTarget?.children[y]?.children[
+            x
+          ] as HTMLDivElement;
+          if (cell) {
+            cell.style.backgroundColor = currentShape.color;
+          }
         }
+      }
     }
+  }
 }
 
+// Функция для удаления предыдущего состояния движущейся фигуры
 function removePreviousShape() {
-    const rowsToColor = currentShape.shape.length
-    const cellsToColor = currentShape.shape[0].length
+  const rowsToClear = currentShape.shape.length;
+  const cellsToClear = currentShape.shape[0].length;
 
-    const centerX = Math.floor((10 - cellsToColor) / 2)
-    const centerY = 0
+  for (let rowIndex = 0; rowIndex < rowsToClear; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < cellsToClear; cellIndex++) {
+      if (currentShape.shape[rowIndex][cellIndex]) {
+        const x = currentX + cellIndex;
+        const y = currentY + rowIndex;
 
-    for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
-        const row = tetrisPlaygroundTarget.children[rowIndex + centerY] as HTMLDivElement
-
-        for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
-            const cell = row.children[cellIndex + centerX] as HTMLDivElement
-            if (currentShape.shape[rowIndex][cellIndex]) {
-                cell.style.backgroundColor = ''
+        // Проверяем, не выходит ли за границы
+        if (x >= 0 && x < 10 && y >= 0 && y < 20) {
+          // Очищаем только если здесь нет зафиксированного блока
+          if (!playground[y][x]) {
+            const cell = tetrisPlaygroundTarget?.children[y]?.children[
+              x
+            ] as HTMLDivElement;
+            if (cell) {
+              cell.style.backgroundColor = '';
             }
+          }
         }
+      }
     }
+  }
 }
 
+// Функция для поворота фигуры на 90 градусов
 function rotateShape(shape: number[][]): number[][] {
-    if (shape.length === 2 && shape[0].length === 2) return shape
+  const rotatedShape: number[][] = [];
+  const rows = shape.length;
+  const cols = shape[0].length;
 
-    const rotatedShape = []
-
-    for (let rowsCount = 0; rowsCount < shape[0].length; rowsCount++) {
-        const row: number[] = []
-        rotatedShape.push(row)
+  for (let col = 0; col < cols; col++) {
+    rotatedShape[col] = [];
+    for (let row = rows - 1; row >= 0; row--) {
+      rotatedShape[col][rows - 1 - row] = shape[row][col];
     }
-    for (let i = shape.length - 1, k = 0; i >= 0; i--, k++) {
-        for (let j = 0; j < shape[0].length; j++) {
-            rotatedShape[j][k] = shape[i][j]
-        }
-    }
+  }
 
-    return rotatedShape
+  return rotatedShape;
 }
 
-renderShape()
+// Функция для перемещения фигуры влево и вправо
+function moveShape(direction: number) {
+  if (!isCollision(currentShape.shape, currentX + direction, currentY)) {
+    removePreviousShape();
+    currentX += direction;
+    renderShape();
+  }
+}
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'Space') {
-        removePreviousShape()
-        currentShape.shape = rotateShape(currentShape.shape)
-        renderShape()
+// Функция для создания игрового поля как массива
+function createPlayground() {
+  const playground = [];
+  for (let row = 0; row < 20; row++) {
+    playground[row] = new Array(10).fill(0);
+  }
+  return playground;
+}
+
+// Функция для генерации новой фигуры
+function generateNewShape() {
+  const shapeKeyIndex = Math.floor(Math.random() * shapeKeys.length);
+  const shapeKey = shapeKeys[shapeKeyIndex] as keyof IShapes;
+  currentShape.shape = shapes[shapeKey].shape;
+  currentShape.color = shapes[shapeKey].color;
+  currentX = 3;
+  currentY = 0;
+}
+
+// Проверка столкновения фигуры с границами или другими фигурами
+function isCollision(shape: number[][], x: number, y: number): boolean {
+  for (let row = 0; row < shape.length; row++) {
+    for (let col = 0; col < shape[row].length; col++) {
+      if (shape[row][col]) {
+        const newX = x + col;
+        const newY = y + row;
+
+        // Проверяем границы поля
+        if (newX < 0 || newX >= 10 || newY >= 20) {
+          return true;
+        }
+
+        // Проверяем на столкновение с зафиксированными блоками
+        if (playground[newY][newX]) {
+          return true;
+        }
+      }
     }
-})
+  }
+  return false;
+}
+
+// Фиксация фигуры на игровом поле
+function fixShape() {
+  for (let row = 0; row < currentShape.shape.length; row++) {
+    for (let col = 0; col < currentShape.shape[row].length; col++) {
+      if (currentShape.shape[row][col]) {
+        playground[currentY + row][currentX + col] = 1;
+      }
+    }
+  }
+}
+
+// Функция для удаления заполненных линий
+function removeFullLines() {
+
+  let rowsToAdd = 0
+  for (let row = playground.length - 1; row >= 0; row--) {
+    if (playground[row].every((cell) => cell === 1)) {
+      playground.splice(row, 1);
+      rowsToAdd++
+    }
+  }
+
+  for(let i = 0; i < rowsToAdd; i++) {
+    playground.unshift(new Array(10).fill(0));
+  }
+
+  renderFixedBlocks();
+}
+
+// === Начало выполнения программы ===
+
+// Инициализируем игровое поле
+const tetrisPlaygroundTarget = document.querySelector(
+  '.tetris-playground'
+) as HTMLDivElement | null;
+
+if (tetrisPlaygroundTarget) {
+  drawTetrisPlayground(10, 20, tetrisPlaygroundTarget);
+}
+
+// Получаем ключи фигур
+const shapeKeys = Object.keys(shapes);
+
+// Инициализируем текущую фигуру
+// TODO: Сделать динамичным
+const currentShape = {
+  shape: shapes['S'].shape,
+  color: shapes['S'].color,
+};
+
+let currentX = 3;
+let currentY = 0;
+let speed = 1000; // Начальная скорость
+let isPaused = false;
+const playground = createPlayground();
+
+
+// Отображаем начальную фигуру
+
+
+
+// Обработчик для кнопки "Пауза"
+const pauseButton = document.getElementById('pauseButton') as HTMLButtonElement;
+pauseButton.addEventListener('click', togglePause);
+
+function togglePause() {
+  isPaused = !isPaused;
+  pauseButton.classList.toggle('paused', isPaused);
+  pauseButton.textContent = isPaused ? 'Продолжить' : 'Пауза';
+  pauseButton.blur();
+}
+
+// Получаем кнопку Старт/Стоп
+const startStopButton = document.getElementById('startStopButton') as HTMLButtonElement;
+
+let gameStarted = false;  // Флаг для отслеживания состояния игры
+
+startStopButton.addEventListener('click', toggleGame);
+
+function toggleGame() {
+  if (!gameStarted) {
+    // Запускаем игру
+    gameStarted = true;
+    startStopButton.classList.toggle('started', gameStarted);
+    startStopButton.textContent = 'Стоп'; // Меняем текст кнопки на Стоп
+    startGame(); // Запускаем игру
+  } else {
+    // Останавливаем игру (сброс)
+    gameStarted = false;
+    startStopButton.classList.remove('started');
+    startStopButton.textContent = 'Старт'; // Меняем текст кнопки на Старт
+    resetGame(); // Сбрасываем игру
+  }
+  startStopButton.blur();
+}
+
+function startGame() {
+  // Инициализация начальных параметров игры и запуск игрового цикла
+  currentX = 3;
+  currentY = 0;
+  playground.length = 0; // Очищаем игровое поле
+  playground.push(...createPlayground()); // Заполняем игровое поле
+  renderFixedBlocks(); // Отображаем зафиксированные блоки (если они есть)
+  generateNewShape(); // Генерируем новую фигуру
+  renderShape(); // Отображаем начальную фигуру
+  gameLoop(); // Запуск основного игрового цикла
+}
+
+
+function resetGame() {
+  // Останавливаем игровой цикл, если он был
+  clearTimeout(gameLoopTimeout); // Останавливаем текущий игровой цикл, если он есть
+  isPaused = false; // Снимаем паузу
+  pauseButton.classList.remove('paused');
+  pauseButton.textContent = 'Пауза';
+  currentShape.shape = shapes['S'].shape;
+  currentShape.color = shapes['S'].color;
+  currentX = 3;
+  currentY = 0;
+  playground.length = 0;
+  playground.push(...createPlayground()); // Заполняем игровое поле
+  renderFixedBlocks();
+}
+
+let gameLoopTimeout: number;
+
+
+// Основной игровой цикл
+function gameLoop() {
+  gameLoopTimeout = setTimeout(() => {
+    if (!isPaused && gameStarted) {
+      if (!isCollision(currentShape.shape, currentX, currentY + 1)) {
+        removePreviousShape();
+        currentY++;
+        renderShape();
+      } else {
+        fixShape();
+        removeFullLines();
+        generateNewShape();
+        if (isCollision(currentShape.shape, currentX, currentY)) {
+          alert('Игра окончена');
+          return;
+        }
+        renderShape();
+      }
+    }
+    if (gameStarted) gameLoop(); // Продолжаем игру только если она начата
+  }, speed);
+}
+
+// Запускаем игровой цикл
+gameLoop();
+
+// Обработчик клавиш для управления фигурой
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Space') {
+    e.preventDefault(); // Предотвращаем стандартное поведение клавиши "Пробел"
+    if (!isPaused) { // Проверяем, не на паузе ли игра
+      const newShape = rotateShape(currentShape.shape);
+      if (!isCollision(newShape, currentX, currentY)) {
+        removePreviousShape();
+        currentShape.shape = newShape;
+        renderShape();
+      }
+    }
+  } else if (e.code === 'ArrowLeft' && !isPaused) {
+    moveShape(-1);
+  } else if (e.code === 'ArrowRight' && !isPaused) {
+    moveShape(1);
+  } else if (e.code === 'ArrowDown' && !isPaused) {
+    speed = 100; // Ускоряем падение фигуры
+  }
+});
+
+
+
+// Обработчик отпускания клавиш
+document.addEventListener('keyup', (e) => {
+  if (e.code === 'ArrowDown') {
+    speed = 1000; // Возвращаем стандартную скорость
+  }
+});
